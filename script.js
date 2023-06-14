51a44539111ac23054d90ed0eea96dd7f2f261c3
Luiz Otávio Dos Santos <luizotavio.03@outlook.com>
2023-06-14 15:28:39 -0300

    Update script.js
    função para alterar a imagem por js criada

--- script.js
+++ script.js
@@ -10,4 +10,17 @@ function toggleMode() {
     // aqui simplifica o código de cima
 
     html.classList.toggle('light')
+
+    // alterar foto do avatar
+    // pegar a tag img
+    const img = document.querySelector("#profile img")
+
+    // substituir a imagem
+    if (html.classList.contains("light")) {
+        // se tiver light mode, add a img light
+        img.setAttribute("src", "./assets/avatar-light.png")
+    } else {
+        // se tiver sem light mode, manter a imagem normal
+        img.setAttribute("src", "./assets/avatar-dark.png")
+    }
 }
